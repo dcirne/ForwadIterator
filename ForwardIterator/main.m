8f548259ca54a108558ba2bf86570ae9c590a8c7
Dalmo Cirne <dalmo.cirne@gmail.com>
2012-08-27 21:32:11 +0200

    Initial Commit

---
+++ ForwardIterator/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ForwardIterator
+//
+//  Created by Dalmo Cirne on 8/27/12.
+//  Copyright (c) 2012 Dalmo Cirne. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
